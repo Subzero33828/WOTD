636c34ac9d102c651aa73fc24e3ca67de94b39cb
Subzero33828 <165351667+Subzero33828@users.noreply.github.com>
2024-10-03 08:20:58 -0400

    Update WOTD_3.1.py

--- WOTD_3.1.py
+++ WOTD_3.1.py
@@ -10,25 +10,25 @@ import schedule
 import time
 
 
-# Constantes pour Twilio et email
+
 TWILIO_ACCOUNT_SID = 'ACe0513e604e5c2e3e7af87582486a1253'
 TWILIO_AUTH_TOKEN = 'ecc19521ddac85ffc83a139b576f5df0'
 FROM_NUMBER = '+1 844 459 0221' 
-TO_NUMBER = '+1 267 282 1075' 
-EMAIL_SENDER = 'subzero33828@gmail.com'
-EMAIL_RECIPIENT = 'shamarbrowne@gmail.com'
+TO_NUMBER = '+1 267 xxx xxx' 
+EMAIL_SENDER = 'xxx@gmail.com'
+EMAIL_RECIPIENT = 'xxx@gmail.com'
 SMTP_SERVER = 'smtp.gmail.com' 
-SMTP_PORT = 587  #??? Define
+SMTP_PORT = 587  
 EMAIL_PASSWORD = 'your_email_password' 
 
-#url = input("Please enter the url to fetch French words: ")  # Ask the user to input the url
+
 url = 'https://www.vistawide.com/french/top_100_french_words.htm'
 
 def fetch_french_words(url: str) -> list[str]:
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, features='html.parser')
-        word_list = soup.find_all('li')  # Ajustez en fonction du site web réel
+        word_list = soup.find_all('li')  
         return [word.get_text() for word in word_list]
     else:
         print("Échec de la récupération des mots.")
@@ -39,17 +39,20 @@ def get_word_of_the_day(words: list[str] ) ->  str:
     if not words:
         print("Aucun mot disponible.")
         return "Aucun mot disponible."
-    return random.choice(seq=words)  # Sélectionnez un mot aléatoire dans la liste
+    return random.choice(words)  
 
 
 def send_sms(word_of_the_day: str):
     try:
         client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-        message = client.messages.create(
+        message = client.messages.create
+        
+        (
             from_=FROM_NUMBER,
             body=f"Le mot du jour d'aujourd'hui est : {word_of_the_day}",
             to=TO_NUMBER
         )
+        
         print(f"Message envoyé : {message:str}")
     except Exception as e:
         print(f"Échec de l'envoi du SMS : {e}")
@@ -64,7 +67,7 @@ def send_email(word_of_the_day: str):
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
-            server.login(user=EMAIL_SENDER, password=EMAIL_PASSWORD)
+            server.login(EMAIL_SENDER, EMAIL_PASSWORD)
             server.sendmail(EMAIL_SENDER, EMAIL_RECIPIENT, msg.as_string())
         print("Email envoyé avec succès.")
     except Exception as e:
@@ -74,25 +77,24 @@ def send_email(word_of_the_day: str):
 def main(url: str) -> None:
     french_words = fetch_french_words(url)
     if french_words:
-        word_of_the_day = get_word_of_the_day(words=french_words)
+        word_of_the_day = get_word_of_the_day(french_words)
         print(f"Mot du jour : {word_of_the_day}")
-       # Represent SMS and email actions as strings
+       
         sms_message = f"Le mot du jour d'aujourd'hui est : {word_of_the_day}"
         email_message = f"Le mot français d'aujourd'hui est : {word_of_the_day}"
 
-        # Log or print the messages instead of sending them
+     
         print(f"SMS to be sent: {sms_message}")
         print(f"Email to be sent: {email_message}")
 
 
-# Planifiez la fonction principale pour qu'elle s'exécute quotidiennement
-schedule.every().day.at("09:00").do(main, url) # type: ignore   # Définissez l'heure à laquelle vous souhaitez que le travail s'exécute
-#schedule.every().day.at("18:00").do(main, url)  # Essai d'ajouter un deuxième message
+
+schedule.every().day.at("09:00").do(main, url) # type: ignore   
 
 if __name__ == "__main__":
     while True:
         schedule.run_pending()
-        time.sleep(60)  # Attendez une minute avant de vérifier à nouveau
+        time.sleep(60) 
 
 
 
